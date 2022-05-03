683934fd3ac9d2a594026e876571ef672f098482
Gustavo Velasco-Hernandez <gustavo.velascoh@gmail.com>
2022-05-03 21:42:47 +0100

    Updated script and notebook

--- dfa-test.py
+++ dfa-test.py
@@ -4,8 +4,16 @@ from urllib.request import urlopen
 from selenium import webdriver
 import time
 from selenium.webdriver.chrome.options import Options
+from random import random
+import traceback
+
+
+random_secs_limit = 15
 
 url = "https://www.dfa.ie/passports/contact/"
+
+time.sleep(random_secs_limit*random())
+
 current_time = time.strftime("%Y-%m-%d %H:%M:%S")
 
 chrome_options = Options()
@@ -13,15 +21,18 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage");
 
+try:
 
-browser = webdriver.Chrome(options=chrome_options)
-browser.get(url)
-time.sleep(5)
-soup = BeautifulSoup(browser.page_source, "html.parser")
+    browser = webdriver.Chrome(options=chrome_options)
+    browser.get(url)
+    time.sleep(5)
+    soup = BeautifulSoup(browser.page_source, "html.parser")
 
-chat_status = soup.find("div",{"id": "chat-button"}).find("img")["alt"]
+    chat_status = soup.find("div",{"id": "chat-button"}).find("img")["alt"]
 
-print("%s, %s" % (current_time, chat_status))
+    print("%s, %s" % (current_time, chat_status))
 
-browser.close()
-browser.quit()
+    browser.close()
+    browser.quit()
+except:
+    print(traceback.format_exc())
